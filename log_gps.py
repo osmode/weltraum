cea468b7bc657b56a502e4b6a7d20cab6eebef63
Omar Metwally <omar.metwally@gmail.com>
2018-06-25 12:01:08 -0700

    log altitude and velocity

--- log_gps.py
+++ log_gps.py
@@ -11,27 +11,47 @@ from time import time
 ser = serial.Serial('/dev/ttyACM0')
 latitude = ''
 longitude = ''
+altitude = ''
+vel = ''
 
 while True:
-        line = str(ser.readline())
-        print('GPS data: ',line)
-        if '$GPGGA' in line:
-            line = line.split(',')
-            print(str(line[2]), str(line[4]))
-            lat_dd = float(str(line[2])[0:2])
-            lat_mm = float(str(line[2])[2:])/60
-            latitude = lat_dd + lat_mm
-            if line[3].lower() == 's':
-                latitude = latitude * -1
+        try:
+                line = str(ser.readline())
+                print('GPS data: ',line)
 
-            long_dd = float(str(line[4])[0:3]) 
-            long_mm = float(str(line[4][3:]))/60
-            longitude = long_dd + long_mm
-            if line[5].lower() == 'w':
-                longitude = longitude * -1
+                if '$GPGGA' in line:
+                    line = line.split(',')
+                    #print(str(line[2]), str(line[4]))
+                    lat_dd = float(str(line[2])[0:2])
+                    lat_mm = float(str(line[2])[2:])/60
+                    latitude = lat_dd + lat_mm
+                    if line[3].lower() == 's':
+                        latitude = latitude * -1
+                    long_dd = float(str(line[4])[0:3]) 
+                    long_mm = float(str(line[4][3:]))/60
+                    longitude = long_dd + long_mm
+                    if line[5].lower() == 'w':
+                        longitude = longitude * -1
+                    print('Longitude: ', longitude)
+                    print('Latitude: ', latitude)
 
-            print('Latitude: ', latitude)
-            print('Longitude: ', longitude)
-            filehandle = open('/home/pi/Desktop/way.csv', 'a')
-            filehandle.write(str(latitude)+', '+str(longitude)+', '+ str(time())+'\n')
-            filehandle.close()
+                    altitude = float(line[9])
+                    print('Altitude: ', altitude, ' meters')
+               
+                if '$GPVTG' in line:
+                    line = line.split(',K')
+                    vel = float(line[0].split(',')[-1])
+                    print('Velocity: ',str(vel),' km/hr')
+
+                if len(str(longitude))*len(str(latitude))*len(str(altitude))*len(str(vel)) > 0:
+                        print('Writing coordinates, altitude, and velocity to file...')
+                        filehandle = open('/home/breitkopf/Desktop/way.csv', 'a')
+                        filehandle.write(str(longitude)+', '+str(latitude)+', '+str(altitude)+', '+str(time())+'\n')
+                        filehandle.close()
+                        longitude = ''
+                        latitude = ''
+                        altitude = ''
+                        vel = ''
+        except:
+                pass
+
